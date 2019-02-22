5030611e5d1b45fd9cb84260a4ed742a26915ec1
nivleM <melvinedward98@gmail.com>
2019-02-22 15:11:46 +0800

    Added weather.html
    Manage reading of response file from api. Response is changed into json file which is read using. JSONfile.(option)

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-// Set up our HTTP request
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+//to be uncommented to use in vscode
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 var API_KEY = "&appid=1afaa7bb7768fa072efe7edd746a72ae";
 var API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var CITY = "London";
 var URL = API_URL.concat(CITY,API_KEY);
+var response;
 
-xhr.onload = function () {
+run();
 
-    // Process our return data
-    if (xhr.status >= 200 && xhr.status < 300) {
-        console.log(xhr.responseText);
-    } else {
-        console.log('The request failed!');
-    }
-};
+function run() {
+    xhr.onload = function () {
 
-xhr.open('GET', URL);
-xhr.send();
+        // Process our return data
+        if (xhr.status >= 200 && xhr.status < 300) {
+            response = xhr.responseText;
+            console.log(response);
+        } else {
+            console.log('The request failed!');
+        }
+    };
+    
+    xhr.open('GET', URL);
+    xhr.send();
+    
+    //delete to run in vs code
+    var myRes = JSON.parse(response);
+    document.getElementById("coordinate").innerHTML = myRes.coord.lon;
+}
+
+
+
+
