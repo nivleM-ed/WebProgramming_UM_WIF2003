33aabc91bbff600b8cf31dff60e115547eb1dda7
nivleM_ <46630582+nivleM-ed@users.noreply.github.com>
2019-04-01 15:07:26 +0800

    Beautify files
    close unclosed tags

--- assets/js/test-checklist.js
+++ assets/js/test-checklist.js
@@ -1,66 +1,62 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
 	var count = 1;
 	var itemId;
 
 	$(".header-btn").click(function () {
 		count++;
 		$(".modi-add").fadeIn(300);
-			
+
 	});
 
 	$(".modi-add .modi-btn-cncl").click(function () {
 		$(".modi-add").fadeOut(300);
 	});
-  
-	$(".modi-add .modi-btn-save").click(function() {
+
+	$(".modi-add .modi-btn-save").click(function () {
 		var value = $(".modi-add .modi-input-field:text").val();
 		console.log(value);
-		$("#item-cl").append(	'<div class="item-container">'+
-        							'<div class="item-box">'+
-          								'<input type="checkbox" class="cl-cb" id="check' + count + '"/>' +
-          								'<label for="check' + count + '"><div><i class="fa fa-check"></i></div>' + value + '</label>' +
-          								'<span class="edit-btn side-btn" for="check' + count + '"></span>' +
-          								'<span class="remv-btn side-btn" for="check' + count + '"></span>' +     
-          							'</div>' +
-								'</div>');
+		$("#item-cl").append('<div class="item-container">' +
+			'<div class="item-box">' +
+			'<input type="checkbox" class="cl-cb" id="check' + count + '"/>' +
+			'<label for="check' + count + '"><div><i class="fa fa-check"></i></div>' + value + '</label>' +
+			'<span class="edit-btn side-btn" for="check' + count + '"></span>' +
+			'<span class="remv-btn side-btn" for="check' + count + '"></span>' +
+			'</div>' +
+			'</div>');
 
-										
 		clRemove(); //allow removal of items added via JQuery
 		clEdit();
 		$(".modi-add").fadeOut(300);
 	});
-  
+
 	clRemove(); //allow removal of initial item before an item is added
 	clEdit();
-  
+
 	function clRemove() {
-		$(".remv-btn").click(function() {
+		$(".remv-btn").click(function () {
 			$(this).parents("div[class*='item-container']").remove();
 			console.log(count);
 		});
 	}
 
-	function clEdit(){
+	function clEdit() {
 		$(".edit-btn").click(function () {
 			$(".modi-edit").fadeIn(300);
-			itemId=$(this).attr('for');
-			var value=$("label[for='" + itemId + "']").text();
+			itemId = $(this).attr('for');
+			var value = $("label[for='" + itemId + "']").text();
 			$(".modi-edit .modi-input-field").val(value);
 			console.log(value);
-		});		
+		});
 	}
 
 	$(".modi-edit .modi-btn-cncl").click(function () {
 		$(".modi-edit").fadeOut(300);
-	});	
+	});
 
 	$(".modi-edit .modi-btn-save").click(function () {
 		var value = $(".modi-input-field:text").val();
 		$(".modi-edit label[for='" + itemId + "']").text(value);
-		
 		$(".modi-edit label[for='" + itemId + "']").prepend("<div><i class='fa fa-check'></i></div>");
-		
 		$(".modi-edit").fadeOut(300);
 		$(".modi-edit .modi-input-field").val("");
 	});
@@ -88,20 +84,12 @@ $(document).ready(function(){
 
 
 $(document).ready(function () {
-	
-	
 
-	
+
+
+
 });
 
 $(document).ready(function () {
-	
+
 });
-
-
-
-
-
-
-
-
